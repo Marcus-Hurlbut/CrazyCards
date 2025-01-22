07835353648a8127a18ba401e74f488645cf90d8
Marcus Hurlbut <marcushurlbut14930@gmail.com>
2025-01-21 20:10:21 -0600

    Adjust configs for deployment

--- src/main/java/com/dungeondecks/marcushurlbut/config/WebConfig.java
+++ src/main/java/com/dungeondecks/marcushurlbut/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply to all endpoints
-                .allowedOrigins("http://localhost:8081") // Allow all origins
+                .allowedOrigins("*") // Allow all origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow credentials if needed
