96703bda2bb69c63320ba8a9a9160d4a4802fe62
Marcus Hurlbut <marcushurlbut14930@gmail.com>
2025-02-06 15:42:13 -0600

    config changes

--- dungeon-decks-app/vue.config.js
+++ dungeon-decks-app/vue.config.js
@@ -1,13 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  chainWebpack: (config) => {
-    config.module
-      .rule('svg')
-      .use('vue-loader')
-      .tap((options) => {
-        options.compilerOptions.isCustomElement = (tag) => tag.startsWith('fe');
-        return options;
-      });
-  }
+  // chainWebpack: (config) => {
+  //   config.module
+  //     .rule('svg')
+  //     .use('vue-loader')
+  //     .tap((options) => {
+  //       options.compilerOptions.isCustomElement = (tag) => tag.startsWith('fe');
+  //       return options;
+  //     });
+  // }
 })
